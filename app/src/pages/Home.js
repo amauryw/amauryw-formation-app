640c1ce8108b69276cff273bb9e5b5694e1edc46
Amaury Wyart <amauryw@MAC-MINI-BAM.theodo.lan>
2018-04-09 17:45:39 +0200

    del HOME & app.js

---
+++ app/src/pages/Home.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import { Text, View, StyleSheet } from "react-native";
+
+export default class Home extends Component {
+  render() {
+    return (
+      <View style={styles.page}>
+        <View style={styles.spacer} />
+        <View style={styles.welcomeContainer}>
+          <Text style={styles.welcomeTitle}>Bienvenue</Text>
+        </View>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  page: {
+    backgroundColor: "white",
+    flex: 1
+  },
+  welcomeContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  welcomeTitle: {
+    color: "#49A698",
+    fontSize: 30
+  },
+  spacer: {
+    flex: 2
+  }
+});
